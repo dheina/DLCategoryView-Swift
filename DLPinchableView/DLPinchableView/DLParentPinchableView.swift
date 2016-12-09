20ef042a63afcb5596e34f046fa7ecf22923f1aa
Dheina Lundi Ahirsya <dheina@11street.my>
2016-12-09 09:58:54 +0800

    Init

---
+++ DLPinchableView/DLPinchableView/DLParentPinchableView.swift
@@ -0,0 +1,13 @@
+//
+//  DLParentPinchableView.swift
+//  DLPinchableView
+//
+//  Created by Dheina Lundi Ahirsya on 08/12/2016.
+//  Copyright © 2016 dheina.com. All rights reserved.
+//
+
+import Cocoa
+
+class DLParentPinchableView: UIView {
+
+}
---
+++ DLPinchableView/DLPinchableView/DLPinchableView.swift
@@ -0,0 +1,13 @@
+//
+//  DLPinchableView.swift
+//  DLPinchableView
+//
+//  Created by Dheina Lundi Ahirsya on 08/12/2016.
+//  Copyright © 2016 dheina.com. All rights reserved.
+//
+
+import Cocoa
+
+class DLPinchableView: UIImageView {
+
+}
